8d94b177b2241ab0988ce1d462210540c076f24c
henrygenus <henry.a.genus@gmail.com>
2019-12-20 23:48:47 -0500

    shorten variable names for single line function declaration

--- Homeworks/HW2/src/eval.h
+++ Homeworks/HW2/src/eval.h
@@ -5,8 +5,7 @@
 #include "Set.h"
 using namespace std;
 
-int evaluate(string infix, const Set& trueValues,
-             const Set& falseValues, string& postfix, bool& result);
+int evaluate(string in, const Set& T, const Set& F, string& post, bool& result);
     // return 3 if char is in true & false, 2 if char is neither, 1 if other error
     // return 0 and set result if successful
 bool toPostFix(string infix, string& postfix);
@@ -15,7 +14,7 @@ bool toPostFix(string infix, string& postfix);
 bool parse(string s);
 bool isHigherPriority(char current, char top);
 int doEval(string postfix, const Set& True, const Set& False, bool &returnValue);
-int getTruthValue(char c, bool &operand, const Set& trueValues, const Set& falseValues);
+int getTruthValue(char c, bool &operand, const Set& True, const Set& False);
 bool operate(bool operand1, char op, bool operand2);
 
 #endif /* eval_h */
